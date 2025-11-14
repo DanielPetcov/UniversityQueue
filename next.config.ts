b9c90ba61dc57ec9c36888bade523cc750c714a0
daniel petcov <danielpetcov.work@gmail.com>
2025-11-14 13:49:06 +0200

    updated

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  outputFileTracingIncludes: {
-    "./app/**/*": [
-      "./app/generated/prisma/**/*",
-      "./node_modules/@prisma/engines/**/*",
-    ],
-  },
+  // outputFileTracingIncludes: {
+  //   "./app/**/*": [
+  //     "./app/generated/prisma/**/*",
+  //     "./node_modules/@prisma/engines/**/*",
+  //   ],
+  // },
 };
 
 export default nextConfig;
